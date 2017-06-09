bb21501ec2ddcd6712aae0d902ed23f7b06abe6e
Ying Yi <ying.yi@sony.com>
2017-06-09 20:11:00 +0100

    Removed the ProgramRepository Pass from the LTOPasses and updated the existing llvm testing.

--- lib/Transforms/IPO/PassManagerBuilder.cpp
+++ lib/Transforms/IPO/PassManagerBuilder.cpp
@@ -906,8 +906,6 @@ void PassManagerBuilder::addLateLTOOptimizationPasses(
   if (MergeFunctions)
     PM.add(createMergeFunctionsPass());
 
-  if (ProgramRepository)
-    PM.add(createProgramRepositoryPass());
 }
 
 void PassManagerBuilder::populateThinLTOPassManager(
